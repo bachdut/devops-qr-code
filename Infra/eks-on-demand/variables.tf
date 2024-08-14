4668523a19bf28eb22d7a25d746054e37bae6d2c
bachdut <76906741+bachdut@users.noreply.github.com>
2024-08-14 17:55:47 +0300

    k8s cluster deployment

---
+++ Infra/eks-on-demand/variables.tf
@@ -0,0 +1,11 @@
+variable "region" {
+  default = "us-west-2"
+}
+
+variable "cluster_name" {
+  default = "on-demand-eks-cluster"
+}
+
+variable "node_group_name" {
+  default = "on-demand-eks-node-group"
+}
